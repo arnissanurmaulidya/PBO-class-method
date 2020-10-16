4fd13b62f94893d24cee6626b26701599e2d7bcd
arnissanurmaulidya <70264816+arnissanurmaulidya@users.noreply.github.com>
2020-10-16 10:33:31 +0700

    Add files via upload

---
+++ Biodata.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ASUS
+ */
+public class Biodata {
+    
+    public void Nama(){
+        System.out.println("Nama : Arnissa nur maulidya");      
+    }
+    
+    public void Absen(){
+        System.out.println("No absen : 04");
+    }
+    
+    public void Kelas(){
+        System.out.println("Kelas : 11 RPL 1");
+    }
+    
+    public void Alamat(){
+        System.out.println("Alamat : Dsn.Tlogorejo, Ds.Wonorejo, Kec.Lawang, Kab.Malang");
+    }
+    
+    public void Lahir(){
+        System.out.println("TTL : MALANG, 01 Februari 2004");
+    }
+    
+    public void Hobi(){
+        System.out.println("Hobi : Nonton drama korea & nonton anime");
+    }
+}
---
+++ Utama.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ASUS
+ */
+public class Utama {
+    public static void main(String [] args){
+        Biodata biodata = new Biodata();
+        biodata.Nama();
+        biodata.Absen();
+        biodata.Kelas();
+        biodata.Alamat();
+        biodata.Lahir();
+        biodata.Hobi();
+    }
+}
